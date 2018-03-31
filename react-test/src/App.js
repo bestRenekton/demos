ab9c5a1d5bd45e9d323cb0108d36f23755f58020
yangyuetao <342060286@qq.com>
2018-03-31 23:05:31 +0800

    r

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './img/logo.svg';
 import './App.css';
 
+
+const numbers=[1,1,2,3]
+
 class Clock extends Component{
   constructor(props){
     super(props)
@@ -40,17 +43,112 @@ class Clock extends Component{
   //   this.setState((prev,prop)=>({num:prev.num+e}))
   // }
   render(){
-    return(
-      <div>
-        <h2>{this.state.date.toLocaleString()}</h2>
-        {/* <p onClick={this.add()}>{this.state.num}</p> */}
-        {/* <p onClick={this.add.bind(this)}>{this.state.num}</p> */}
-        <p onClick={()=>{this.add(1)}}>{this.state.num}</p>
-      </div>
-    )
+    if(this.props.show){
+      return(
+        <div>
+          <h2>{this.state.date.toLocaleString()}</h2>
+          <p onClick={()=>{this.add(1)}}>{this.state.num}</p>
+          <ul>
+            {this.props.num.map((e,i)=>{
+              return <li key={i}>{e}</li>
+            })}
+          </ul>
+        </div>
+      )
+    }else{
+      return(
+        <div>
+          <h2>{this.state.date.toLocaleString()}</h2>
+        </div>
+      )
+    }
   }
 }
+//选择
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite La Croix flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+//多个input
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    console.log(name)
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+        <input type="text" value="asdfsdf"/>
+        <input type="text" value={null}/>
+      </form>
+    );
+  }
+}
 class App extends Component {
   constructor(props){
     super(props)
@@ -76,8 +174,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Clock />
-        <Clock />
+        <Clock show={false} num={numbers} />
+        <Clock show={true} num={numbers} />
+        <FlavorForm />
+        <Reservation />
       </div>
     );
   }
