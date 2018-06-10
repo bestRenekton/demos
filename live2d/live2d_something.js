e6e3821b8a430b251d84b5b2f2d8fd0df94b0304
yangyuetao <342060286@qq.com>
2018-06-10 21:07:20 +0800

    live2d

---
+++ live2d/live2d_something.js
@@ -0,0 +1,27 @@
+let clothesNum = Math.floor(Math.random() * 7);
+
+const changeClothes = () => {
+    let newClothesNum = Math.floor(Math.random() * 7);
+
+    if (newClothesNum == clothesNum) {
+        changeClothes();
+    } else {
+        clothesNum = newClothesNum;
+        if (newClothesNum == 0) {
+            loadlive2d("live2d", `./tia/model0.json`);
+        } else if (newClothesNum == 1) {
+            loadlive2d("live2d", `./tia/model1.json`);
+        } else if (newClothesNum == 2) {
+            loadlive2d("live2d", `./tia/model2.json`);
+        } else if (newClothesNum == 3) {
+            loadlive2d("live2d", `./pio/model.json`);
+        } else if (newClothesNum == 4) {
+            loadlive2d("live2d", `./z16/z16.model.json`);
+        } else if (newClothesNum == 5) {
+            loadlive2d("live2d", `./epsilon/Epsilon2.1.model.json`);
+        } else if (newClothesNum == 6) {
+            loadlive2d("live2d", `./remu/rem.json`);
+        }
+    }
+}
+
