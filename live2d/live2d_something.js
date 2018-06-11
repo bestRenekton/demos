b011b630330fb06f7f63b5d35b8b64546b05106d
yyt <342060286@qq.com>
2018-06-11 15:02:05 +0800

    j

--- live2d/live2d_something.js
+++ live2d/live2d_something.js
@@ -14,7 +14,7 @@ const changeClothes = () => {
         } else if (newClothesNum == 2) {
             loadlive2d("live2d", `./tia/model2.json`);
         } else if (newClothesNum == 3) {
-            loadlive2d("live2d", `./pio/model.json`);
+            loadlive2d("live2d", `./pio/0.json`);
         } else if (newClothesNum == 4) {
             loadlive2d("live2d", `./z16/z16.model.json`);
         } else if (newClothesNum == 5) {
